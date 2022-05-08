122689adf87ad78249dcfcfed399885cf6fcb015
Dylan King <mountainboydk@gmail.com>
2022-05-08 15:44:34 -0700

    create  class which stores endpoint

--- mediawiki/lib.py
+++ mediawiki/lib.py
@@ -1,4 +1,3 @@
-
 import aiohttp
 import asyncio
 
@@ -7,8 +6,7 @@ from typing import Union
 from typing import Optional
 from typing import overload
 from collections.abc import Iterable
-
-EN_WIKIPEDIA_ENDPOINT = "https://en.wikipedia.org/w/api.php?"
+from dataclasses import dataclass
 
 def _join_params(params: Iterable[str]) -> str:
     # FIXME: if params need to have '|' in them, use other seperation method according to information here:
@@ -17,22 +15,34 @@ def _join_params(params: Iterable[str]) -> str:
     # rules.
     return '|'.join(params)
 
-@overload
-async def summary(titles: str) -> Optional[str]:
-    ...
 
-@overload
-async def summary(titles: list[str]) -> dict[str, Optional[str]]:
-    ...
+@dataclass(frozen=True)
+class Wiki:
+    _endpoint: str
 
-async def summary(titles: Union[str, list[str]]) -> Union[Optional[str], dict[str, Optional[str]]]:
-    call = {
-        'action' : 'query',
-            'prop' : 'extracts',
-                'exintro' : True,
-                'explaintext' : True,
-            'titles' : titles if type(titles) is str else _join_params(titles),
-        'format' : 'json',
-    }
-    async with aiohttp.ClientSession() as session, session.get(EN_WIKIPEDIA_ENDPOINT + urlencode(call)) as response:
-        return await response.json()
+    @overload
+    async def summary(self, titles: str) -> Optional[str]:
+        ...
+    
+    @overload
+    async def summary(self, titles: list[str]) -> dict[str, Optional[str]]:
+        ...
+    
+    async def summary(self, titles: Union[str, list[str]]) -> Union[Optional[str], dict[str, Optional[str]]]:
+        call = {
+            'action' : 'query',
+                'prop' : 'extracts',
+                    'exintro' : True,
+                    'explaintext' : True,
+                'titles' : titles if type(titles) is str else _join_params(titles),
+            'format' : 'json',
+        }
+        async with aiohttp.ClientSession() as session, session.get(self._endpoint + '?' + urlencode(call)) as response:
+            return await response.json()   
+    
+EN_WIKIPEDIA = Wiki("https://en.wikipedia.org/w/api.php")
+summary = EN_WIKIPEDIA.summary
+
+
+
+
